54777a1c08d5005daa945187cfa05e65cb571527
depeckerd <denis.depecker@cgi.com>
2016-11-19 18:05:23 +0100

    Mise à jour de Dice pour qu'il prenne les bons formats.

--- src/main/java/dad/battle/Dice.java
+++ src/main/java/dad/battle/Dice.java
@@ -8,26 +8,41 @@ import java.util.regex.Pattern;
 public class Dice {
 
 	private List<Die> handOfDice = new ArrayList<>();
-	private static final Pattern HAND_PATTERN = Pattern.compile("^(?<numberOfDice>\\d+)d(?<numberOfFaces>\\d+)$");
-	
+	private int modifier = 0;
+	private static final Pattern HAND_PATTERN = Pattern.compile("^\\d+d\\d+(\\+\\d+d\\d+)*([+-]\\d+)?$");
+	private static final Pattern GROUP_PATTERN = Pattern.compile("(?<numberOfDice>\\d+)d(?<numberOfFaces>\\d+)");
+	private static final Pattern MODIFIER_PATTERN = Pattern.compile("(?<modifier>[+-]\\d+)$");
+
 	public Dice(String hand) {
+		// Vérification de la syntaxe globale.
 		Matcher handMatcher = HAND_PATTERN.matcher(hand);
 		if (!handMatcher.matches()) {
 			throw new IllegalArgumentException(hand);
 		}
-		int numberOfDice = Integer.parseInt(handMatcher.group("numberOfDice"));
-		int numberOfFaces = Integer.parseInt(handMatcher.group("numberOfFaces"));
+
+		// Récupération des groupes de dés.
+		Matcher groupMatcher = GROUP_PATTERN.matcher(hand);
+		while (groupMatcher.find()) {
+			int numberOfDice = Integer.parseInt(groupMatcher.group("numberOfDice"));
+			int numberOfFaces = Integer.parseInt(groupMatcher.group("numberOfFaces"));
+
+			for (int i = 0; i < numberOfDice; ++i) {
+				handOfDice.add(Die.get(numberOfFaces));
+			}
+		}
 		
-		for (int i = 0; i < numberOfDice; ++i) {
-			handOfDice.add(Die.get(numberOfFaces));
+		// Récupération du modificateur.
+		Matcher modifierMatcher = MODIFIER_PATTERN.matcher(hand);
+		if (modifierMatcher.find()) {
+			modifier = Integer.parseInt(modifierMatcher.group("modifier"));
 		}
 	}
-	
+
 	public int roll() {
 		int result = 0;
 		for (Die d : handOfDice) {
 			result += d.roll();
 		}
-		return result;
+		return result + modifier;
 	}
 }
--- src/test/java/dad/battle/DiceTest.java
+++ src/test/java/dad/battle/DiceTest.java
@@ -1,12 +1,12 @@
 package dad.battle;
 
 import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.containsInAnyOrder;
 import static org.hamcrest.Matchers.greaterThanOrEqualTo;
+import static org.hamcrest.Matchers.hasItems;
 import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.lessThanOrEqualTo;
 import static org.hamcrest.Matchers.not;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertThat;
 
 import java.util.HashSet;
@@ -49,24 +49,90 @@ public class DiceTest {
 		assertThat("Il n'y a pas 11 valeurs", rolls, hasSize(11));
 	}
 
-	@Test
-	public void testFormat() {
-		Dice d = null;
-		try {
-			d = null;
-			d = new Dice("2d6*8");
-		} catch (IllegalArgumentException e) {
-
-		}
-		assertNull("Il n'y a pas eu d'exception", d);
-
-		try {
-			d = null;
-			d = new Dice("2d6");
-		} catch (IllegalArgumentException e) {
-
-		}
-		assertNotNull("Il y a eu une exception", d);
+	@Test(expected = IllegalArgumentException.class)
+	public void testIncorrectFormat1() {
+		new Dice("2d6*8");
 	}
 
+	@Test(expected = IllegalArgumentException.class)
+	public void testIncorrectFormat2() {
+		new Dice("2d6a");
+	}
+
+	@Test
+	public void testCorrectFormat1() {
+		new Dice("2d6");
+	}
+
+	@Test
+	public void testRollFormat1() {
+		Dice d = new Dice("2d6");
+		for (int i = 0; i < 100; i++) {
+			assertThat("Le jeté n'est pas bon", d.roll(), allOf(greaterThanOrEqualTo(2), lessThanOrEqualTo(12)));
+		}
+
+	}
+
+	@Test
+	public void testCorrectFormat2() {
+		new Dice("2d6+3d8");
+	}
+
+	@Test
+	public void testRollFormat2() {
+		Dice d = new Dice("2d6+3d8");
+		for (int i = 0; i < 100; i++) {
+			assertThat("Le jeté n'est pas bon", d.roll(), allOf(greaterThanOrEqualTo(5), lessThanOrEqualTo(36)));
+		}
+	}
+	
+	@Test
+	public void testRollRepartitionFormat2() {
+		Dice d = new Dice("2d6+3d8");
+		Set<Integer> results = new HashSet<>();
+		for (int i = 0; i < 10000; i++) {
+			results.add(d.roll());
+		}
+		assertThat("Le jeté n'est pas bon", results, hasItems(8, 10, 20, 33));
+	}
+
+	@Test
+	public void testCorrectFormat3() {
+		new Dice("1d8+1");
+	}
+	
+	@Test
+	public void testRollRepartitionFormat3() {
+		Dice d = new Dice("1d8+1");
+		Set<Integer> results = new HashSet<>();
+		for (int i = 0; i < 1000; i++) {
+			results.add(d.roll());
+		}
+		assertThat("Le jeté n'est pas bon", results, containsInAnyOrder(2, 3, 4, 5, 6, 7, 8, 9));
+	}
+
+	@Test
+	public void testCorrectFormat4() {
+		new Dice("2d6+1d12+3");
+	}
+	
+	@Test
+	public void testRollRepartitionFormat4() {
+		Dice d = new Dice("2d6+1d12+3");
+		Set<Integer> results = new HashSet<>();
+		for (int i = 0; i < 10000; i++) {
+			results.add(d.roll());
+		}
+		assertThat("Le jeté n'est pas bon", results, hasItems(6, 10, 27));
+	}
+	
+	@Test
+	public void testRollRepartitionFormat5() {
+		Dice d = new Dice("1d8-1");
+		Set<Integer> results = new HashSet<>();
+		for (int i = 0; i < 1000; i++) {
+			results.add(d.roll());
+		}
+		assertThat("Le jeté n'est pas bon", results, containsInAnyOrder(0, 1, 2, 3, 4, 5, 6, 7));
+	}
 }
